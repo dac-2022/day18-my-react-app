289890c51917982d2a9eb021a3873ff6b9f36115
SANTOSH MONDAL <santoshmondal@users.noreply.github.com>
2022-06-26 11:05:03 +0530

    working with event. how to call functions in React.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
+  // Event Function
+  const clickMe1 = (p1) => {
+    console.log(p1);
+    alert(`Hello World ${p1}`);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Event Binding Demo</h1>
+
+      {/** This is shourcut */}
+      <input type="button" value="Click Me" onClick={clickMe1} />
+
+      {/** Internally React Convert into this. */}
+      <input type="button" value="Click Mee" onClick={(e) => clickMe1(e)} />
+
+      {/** This approach is very important, when we need to pass parameter */}
+      <input type="button" value="Click Me 1" onClick={() => clickMe1()} />
+
+      {/** Pass Parameter to function */}
+      <input
+        type="button"
+        value="Click Me 2"
+        onClick={() => clickMe1("Rohit")}
+      />
     </div>
   );
 }
