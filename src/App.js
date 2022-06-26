1e336121b780d811d1190c9554f30507d6749d04
SANTOSH MONDAL <santoshmondal@users.noreply.github.com>
2022-06-26 15:00:05 +0530

    basic ajax integration with react.

--- src/App.js
+++ src/App.js
@@ -1,72 +1,26 @@
-import {
-  Alert,
-  Badge,
-  Button,
-  Container,
-  Nav,
-  Navbar,
-  NavDropdown,
-} from "react-bootstrap";
+import axios from "axios";
+import { useEffect, useRef, useState } from "react";
 
 function App() {
+  let [list, setList] = useState([]);
+
+  let callAjaxNow = async () => {
+    const url = "https://jsonplaceholder.typicode.com/posts";
+    let res = await axios.get(url);
+
+    // react specifc, updating the statful variable.
+    const newList = [...res.data];
+    setList(newList);
+  };
+
   return (
     <div>
-      <Navbar bg="light" expand="lg">
-        <Container fluid>
-          <Navbar.Brand href="#">Navbar scroll</Navbar.Brand>
-          <Navbar.Toggle aria-controls="navbarScroll" />
-          <Navbar.Collapse id="navbarScroll">
-            <Nav
-              className="me-auto my-2 my-lg-0"
-              style={{ maxHeight: "100px" }}
-              navbarScroll
-            >
-              <Nav.Link href="#action1">Home</Nav.Link>
-              <Nav.Link href="#action2">Link</Nav.Link>
-              <NavDropdown title="Link" id="navbarScrollingDropdown">
-                <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
-                <NavDropdown.Item href="#action4">
-                  Another action
-                </NavDropdown.Item>
-                <NavDropdown.Divider />
-                <NavDropdown.Item href="#action5">
-                  Something else here
-                </NavDropdown.Item>
-              </NavDropdown>
-              <Nav.Link href="#" disabled>
-                Link
-              </Nav.Link>
-            </Nav>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
+      <h1>Hello Life Cycle</h1>
+      <input type="button" value="Call Ajax Now" onClick={callAjaxNow} />
 
-      <hr />
-      <Navbar bg="dark" variant="dark">
-        <Container>
-          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
-          <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
-          </Nav>
-        </Container>
-      </Navbar>
-
-      <h1>Hello World</h1>
-
-      <span className="badge bg-dark">100</span>
-      <Badge bg="dark">100</Badge>
-
-      <hr />
-
-      <input type="button" value="Click Me" className="btn btn-info" />
-      <Button variant="info">Click Me</Button>
-
-      <hr />
-
-      <div className="alert alert-info">Hello</div>
-      <Alert variant="info">Hello</Alert>
+      {list.map((item, index) => (
+        <div key={index}>{item.title}</div>
+      ))}
     </div>
   );
 }
