8681802c4b939401283e5499f13ec945ffbabd5d
SANTOSH MONDAL <santoshmondal@users.noreply.github.com>
2022-06-26 12:21:56 +0530

    b5 bootstrap.

--- src/App.js
+++ src/App.js
@@ -1,50 +1,7 @@
-import { useState } from "react";
-
 function App() {
-  let [login, setLogin] = useState(false);
   return (
     <div>
-      <h1>Conditional Demo</h1>
-      {login ? (
-        <LoggedInUser setLogin={setLogin} />
-      ) : (
-        <NotLoggedInUser setLogin={setLogin} />
-      )}
-    </div>
-  );
-}
-
-function LoggedInUser({ setLogin }) {
-  const changeLoginStatus = () => {
-    setLogin(false);
-  };
-
-  return (
-    <div>
-      <h1>User is Logged In</h1>
-
-      <h1>Home Page</h1>
-      <input type="button" value="Logout" onClick={changeLoginStatus} />
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. A doloremque
-        architecto omnis! Maxime placeat expedita nesciunt, aperiam dicta
-        deserunt illum numquam veniam ullam dolorum maiores quod! Illum,
-        veritatis! Quibusdam, dolores!
-      </p>
-    </div>
-  );
-}
-
-function NotLoggedInUser({ setLogin }) {
-  const changeLoginStatus = () => {
-    setLogin(true);
-  };
-
-  return (
-    <div>
-      <h1>User is Not Logged In</h1>
-      <h1>Login Page</h1>
-      <input type="button" value="Login" onClick={changeLoginStatus} />
+      <h1>hello World</h1>
     </div>
   );
 }
