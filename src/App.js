cb13ab92eeb21544cf5340a350f4a7745f27bbe4
SANTOSH MONDAL <santoshmondal@users.noreply.github.com>
2022-06-26 11:51:05 +0530

    login and loguout componetnt.

--- src/App.js
+++ src/App.js
@@ -2,47 +2,49 @@ import { useState } from "react";
 
 function App() {
   let [login, setLogin] = useState(false);
+  return (
+    <div>
+      <h1>Conditional Demo</h1>
+      {login ? (
+        <LoggedInUser setLogin={setLogin} />
+      ) : (
+        <NotLoggedInUser setLogin={setLogin} />
+      )}
+    </div>
+  );
+}
 
+function LoggedInUser({ setLogin }) {
   const changeLoginStatus = () => {
-    /*if (login) {
-      setLogin(false);
-    } else {
-      setLogin(true);
-    }*/
-    login ? setLogin(false) : setLogin(true);
+    setLogin(false);
   };
 
   return (
     <div>
-      <h1>Conditional Block</h1>
+      <h1>User is Logged In</h1>
 
-      {login ? "You are logged In" : "Please Login!"}
-      {login ? <h1>you are logged in</h1> : <h1>Please Login!</h1>}
+      <h1>Home Page</h1>
+      <input type="button" value="Logout" onClick={changeLoginStatus} />
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. A doloremque
+        architecto omnis! Maxime placeat expedita nesciunt, aperiam dicta
+        deserunt illum numquam veniam ullam dolorum maiores quod! Illum,
+        veritatis! Quibusdam, dolores!
+      </p>
+    </div>
+  );
+}
 
-      {/** IF  */}
-      {!login && (
-        <div>
-          <p>
-            Login Page. Please login with your creedntials. ELse create an
-            Account wiht us.
-          </p>
-          <input type="button" value="Login" onClick={changeLoginStatus} />
-        </div>
-      )}
+function NotLoggedInUser({ setLogin }) {
+  const changeLoginStatus = () => {
+    setLogin(true);
+  };
 
-      {/** IF */}
-      {login && (
-        <div>
-          <p>
-            Home Page :: Lorem ipsum dolor sit amet consectetur adipisicing
-            elit. Ducimus minima, velit totam et asperiores laborum assumenda
-            omnis voluptates. Vitae consectetur sequi natus cupiditate non
-            officia veniam recusandae suscipit illo doloribus.
-          </p>
-
-          <input type="button" value="Logout" onClick={changeLoginStatus} />
-        </div>
-      )}
+  return (
+    <div>
+      <h1>User is Not Logged In</h1>
+      <h1>Login Page</h1>
+      <input type="button" value="Login" onClick={changeLoginStatus} />
     </div>
   );
 }
